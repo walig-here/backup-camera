c2f8194be4b664e33b186237473059b3bb16586a
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2024-05-12 19:07:02 +0200

    Prototype of saturation, brightness and contrast changing function for UI preprocessing.

--- backup_camera/_image_processing/_preprocessor.py
+++ backup_camera/_image_processing/_preprocessor.py
@@ -6,6 +6,7 @@ się na obrazie elementów. Druga powinna być przystosowana do prezentacji
 na interfejsie graficznym (zastosowanie efektów dot. jasności, kontrastu, 
 saturacji itd.). 
 """
+import numpy as np
 import cv2 as cv
 from cv2.typing import MatLike
 
@@ -17,8 +18,45 @@ class Preprocessor:
         return (self._preprocess_for_ui(frame), self._preprocess_for_classifier(frame))
     
     def _preprocess_for_ui(self, frame):
+        frame = apply_saturation(frame, 0)
+        frame = apply_brightness_contrast(frame, 0, 0)
         return frame
     
     def _preprocess_for_classifier(self, frame):
         return frame
+
+
+
+def apply_saturation(frame: MatLike, saturation_value: int) -> MatLike:
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    frame[:,:,1] = np.clip(frame[:,:,1] + saturation_value, 0, 255)
+    frame = cv.cvtColor(frame, cv.COLOR_HSV2BGR)
+
+    return frame
+    
+    
+def apply_brightness_contrast(frame, brightness=0.0, contrast=0.0):
+
+    if brightness != 0:
+        if brightness > 0:
+            shadow = brightness
+            highlight = 255
+        else:
+            shadow = 0
+            highlight = 255 + brightness
+        alpha_b = (highlight - shadow) / 255
+        gamma_b = shadow
+            
+        buf = cv.addWeighted(frame, alpha_b, frame, 0, gamma_b)
+    else:
+        buf = frame.copy()
+        
+    if contrast != 0:
+        f = 131*(contrast + 127)/(127*(131-contrast))
+        alpha_c = f
+        gamma_c = 127*(1-f)
+            
+        buf = cv.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+
+    return buf
     
